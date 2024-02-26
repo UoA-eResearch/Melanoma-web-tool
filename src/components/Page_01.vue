5063d0bdd266996e9046ae466b0ea64ca18e9844
tjay723 <tjay723@aucklanduni.ac.nz>
2024-02-26 13:59:33 +1300

    element_file_jsons

--- src/components/Page_01.vue
+++ src/components/Page_01.vue
@@ -8,7 +8,16 @@
         </el-tabs>
 
         <Teleport to="body">
+            <!-- use the modal component, pass in the prop -->
             <modal :show="showModal" @close="showModal = false">
+                <!-- <template #header>
+              <h3>Sample Header</h3>
+            </template>
+            <template #body>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque. Est sit amet facilisis magna etiam tempor orci. Augue eget arcu dictum varius duis. Rhoncus urna neque viverra justo nec ultrices dui. Et egestas quis ipsum suspendisse ultrices gravida. Elit ut aliquam purus sit amet luctus venenatis lectus magna. Nisl nunc mi ipsum faucibus. Ultrices eros in cursus turpis massa tincidunt dui ut. Turpis egestas pretium aenean pharetra magna ac placerat. Dictum sit amet justo donec enim diam vulputate. Netus et malesuada fames ac. Urna condimentum mattis pellentesque id nibh tortor id aliquet. Dolor magna eget est lorem. Dui id ornare arcu odio ut sem. Ipsum dolor sit amet consectetur. Orci sagittis eu volutpat odio facilisis mauris sit.
+              </p>
+            </template> -->
                 <template #header>
                     <h3>STATISTICS - {{selectedElm}}</h3>
                 </template>
@@ -38,108 +47,152 @@
 </template>
 
 <script>
-    import { ScaffoldVuer } from '@abi-software/scaffoldvuer';
-    // Import other necessary components and libraries
+/* eslint-disable no-alert, no-console */
+import { ScaffoldVuer } from '@abi-software/scaffoldvuer';
+import Modal from './Modal.vue';
+import data_elements from '@/data/data_elements.json';
+export default {
+  name: 'page_01',
+  components: {
+    ScaffoldVuer,
+    Modal
+  },
+  data: function () {
+    return {
+      showModal: false,
+      tabPosition: 'left',
+      selected: " Selected",
+      displays: [
 
-    export default {
-        name: 'page_01',
-        components: {
-            ScaffoldVuer,
-            // Other components
-        },
-        data() {
-            return {
-                // Your data properties
-            };
-        },
-        methods: {
-            onReady() {
-                this.$nextTick(() => {
-                    const scene = this.$refs.scaffold.$module.scene;
-                    const rootRegion = scene.getRootRegion();
-                    const regions = rootRegion.getChildRegions();
-                    regions.forEach(region => {
-                        region.setVisibility(false);
-                    });
-                });
-            },
-            toggleRegionVisibility(region_name, visibility) {
-                this.$nextTick(() => {
-                    const scene = this.$refs.scaffold.$module.scene;
-                    const rootRegion = scene.getRootRegion();
-                    const region = rootRegion.getChildWithName(region_name);
-                    if (region) {
-                        region.setVisibility(visibility);
-                    } else {
-                        //console.error(`Region with name ${region_name} not found.`);
-                    }
-                });
-            },
-            displayLabelWithGlyph() {
-                // Implement logic to display labels with glyphs
-            }
-        }
+
+        { type: 'scaffold', url: "/data/all_element_jasons/element_all.json", ref: "Skin Selection Tool" },
+        { type: 'scaffold', url: "/data/skin_cancer_metadata1.json", ref: "Skin Cancer" },
+      ],
+      scaffoldsArray: [],
+      selectedElm: '',
+      selectedElmData: [],
+      data_elements: data_elements
     }
+  },
+  methods: {
+    onSelected: function (response) {
+      const { data: { id } } = response?.[0];
+      
+      this.selectedElmData = this.data_elements[id];
+      this.selectedElm = id;
+      this.showModal = true;
+
+    },
+    RemoveModel: function () {
+      var currentLength = this.displays.length;
+      if (currentLength > 0)
+        var removed = this.displays.shift()
+      if (removed.type === 'scaffold') {
+        this.scaffoldsArray.unshift(removed)
+      }
+      else if (removed.type === 'plot') {
+        this.csvFiles.unshift(removed)
+      }
+
+    },
+    ScaffoldSelected: function (annotation) {
+      this.selected = annotation[0].data.id;
+    },
+    AddModel: function () {
+      if (this.scaffoldsArray.length)
+        this.displays.push(this.scaffoldsArray.shift());
+    },
+    AddData: function () {
+      if (this.csvFiles.length)
+        this.displays.push(this.csvFiles.shift());
+     },
+     onReady() {
+         this.$nextTick(() => {
+             const scene = this.$refs.scaffold.$module.scene;
+             const rootRegion = scene.getRootRegion();
+             const regions = rootRegion.getChildRegions();
+             regions.forEach(region => {
+                 region.setVisibility(false);
+             });
+         });
+     },
+     toggleRegionVisibility(region_name, visibility) {
+         this.$nextTick(() => {
+             const scene = this.$refs.scaffold.$module.scene;
+             const rootRegion = scene.getRootRegion();
+             const region = rootRegion.getChildWithName(region_name);
+             if (region) {
+                 region.setVisibility(visibility);
+             } else {
+                 //console.error(`Region with name ${region_name} not found.`);
+             }
+         });
+      },
+      displayLabelWithGlyph() {
+          // Implement logic to display labels with glyphs
+      }
+  }
+  }
+}
 </script>
 
-
 <style>
-    body {
-        margin: 0px
-    }
+body {
+  margin: 0px
+}
 
-    #p1 {
-        padding: 0;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        height: 100vh;
-        width: 100%;
-        position: absolute;
-    }
+#p1 {
+  padding: 0;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  height: 100vh;
+  width: 100%;
+  position: absolute;
+}
 
-    .top-panel {
-        padding: 1%;
-        height: 4%;
-    }
+.top-panel {
+  padding: 1%;
+  height: 4%;
+}
 
-    .my-button {
-        margin: 5px;
-    }
+.my-button {
+  margin: 5px;
+}
 
-    .el-tabs__content {
-        height: 100%;
-    }
+.el-tabs__content {
+  height: 100%;
+}
 
-    table {
-        width: 100%;
-        margin: 20px auto;
-        table-layout: auto;
-    }
+table {
+  width: 100%;
+  margin: 20px auto;
+  table-layout: auto;
+}
 
-    .fixed {
-        table-layout: fixed;
-    }
+.fixed {
+  table-layout: fixed;
+}
 
-    table,
-    td,
-    th {
-        border-collapse: collapse;
-    }
+table,
+td,
+th {
+  border-collapse: collapse;
+}
 
-    th,
-    td {
-        padding: 10px;
-        border: solid 1px;
-        text-align: center;
-    }
+th,
+td {
+  padding: 10px;
+  border: solid 1px;
+  text-align: center;
+}
 
-    .left-align {
-        text-align: left;
-    }
+.left-align {
+  text-align: left;
+}
 
-    .right-align {
-        text-align: right;
-    }
+.right-align {
+  text-align: right;
+}
 </style>

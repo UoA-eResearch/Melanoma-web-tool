25b01e1d2eefe23c4dc503e6e19b4fd7f9493b8a
tjay723 <tjay723@aucklanduni.ac.nz>
2024-03-20 11:15:17 +1300

    add region data

--- src/components/Page_01.vue
+++ src/components/Page_01.vue
@@ -3,39 +3,43 @@
         <el-tabs :tab-position="tabPosition" style="top:10%;height: 90%;">
             <el-tab-pane v-for="item in displays" :key="item.ref" :label="item.ref" style="height:100%">
                 <ScaffoldVuer @on-ready="onReady" @scaffold-selected="onSelected" v-if="item.type === 'scaffold'"
-                    :url="item.url" v-on:scaffold-selected="ScaffoldSelected" :ref="item.ref" :style="{ 'margin-right': showSidebar ? sidebarWidth : '0', 'height': '100%' }" />
+                    :url="item.url" v-on:scaffold-selected="ScaffoldSelected" :ref="item.ref"
+                    :style="{ 'margin-right': showSidebar ? sidebarWidth : '0', 'height': '100%' }" />
                 <div v-show="showSidebar">
                     <div class="sidebar" :class="{ 'show': showSidebar }">
                         <button @click="showSidebar = false">Hide STATISTICS</button>
                         <h3 v-if="selectedElm">STATISTICS - {{ selectedElm }}</h3>
-                        <table v-if="selectedElmData">
-                            <thead>
-                                <th class="right-align">Code</th>
-                                <th>Nodefield</th>
-                                <th class="right-align"># Cases</th>
-                                <th class="right-align">Mean Drainage % </th>
-                                <th class="right-align">95%CI</th>
-                                <th v-if="showTISColumn" class="right-align">TIS</th> <!-- Add v-if directive to conditionally render TIS column -->
-                            </thead>
-                            <tbody>
-                                <tr v-for="elm in selectedElmData" v-bind:key="elm.id">
-                                    <td class="right-align">{{ elm.code }}</td>
-                                    <td>{{ elm.name }}</td>
-                                    <td class="right-align">{{ elm.count }}</td>
-                                    <td class="right-align">{{ elm.percentage }}</td>
-                                    <td class="right-align">{{ elm.CI }}</td>
-                                    <td v-if="showTISColumn && (elm.code === 'ltis' || elm.code === 'rtis')" class="right-align">TIS</td> <!-- Show TIS if code is ltis or rtis -->
-                                </tr>
-                            </tbody>
-                        </table>
+                        <div v-if="selectedElmData">
+                            <table>
+                                <thead>
+                                    <th class="right-align">Code</th>
+                                    <th>Nodefield</th>
+                                    <th class="right-align"># Cases</th>
+                                    <th class="right-align">Mean Drainage % </th>
+                                    <th class="right-align">95%CI</th>
+
+                                </thead>
+                                <tbody>
+                                    <tr v-for="elm in selectedElmData" v-bind:key="elm.id">
+                                        <td class="right-align">{{ elm.code }}</td>
+                                        <td>{{ elm.name }}</td>
+                                        <td class="right-align">{{ elm.count }}</td>
+                                        <td class="right-align">{{ elm.percentage }}</td>
+                                        <td class="right-align">{{ elm.CI }}</td>
+
+                                    </tr>
+                                </tbody>
+                            </table>
+                            
+                            <div class="right-align">TIS = Triangular Intermuscular Space </div>
+                        </div>
                         <span v-else>No data available!</span>
-                        
-                    
+
+
                     </div>
                 </div>
             </el-tab-pane>
         </el-tabs>
-        <p v-if="showTISColumn && hasTISInData">* TIS = Triangular Intermuscular Space</p> <!-- Show TIS explanation if TIS is present in data -->
     </div>
 </template>
 
@@ -54,7 +58,7 @@ export default {
             sidebarWidth: '20%',
             tabPosition: 'left',
             selected: " Unselected",
-            
+
             displays: [
 
 
@@ -65,7 +69,7 @@ export default {
             selectedElm: '',
             selectedElmData: [],
             data_elements: data_elements,
-            
+
         }
     },
     methods: {
@@ -75,7 +79,7 @@ export default {
             this.selectedElmData = this.data_elements[id];
             this.selectedElm = id;
             this.showSidebar = true;
-            
+
 
             // Toggle region visibility
             this.displays.forEach(item => {
@@ -98,19 +102,19 @@ export default {
                 this.displays.push(this.csvFiles.shift());
         },
         onReady() {
-             console.log('zoom out');
+            console.log('zoom out');
             this.$refs['Skin Selection Tool'][0].$module.scene.viewAll();
             this.displays.forEach(item => {
                 if (this.$refs[item.ref] && this.$refs[item.ref].$module) {
-                const scene = this.$refs[item.ref].$module.scene;
-                const rootRegion = scene.getRootRegion();
-                const regions = rootRegion.getChildRegions();
-                regions.forEach(region => {
-                    region.setVisibility(false);
-                });
-            }
-            }); 
-            setTimeout (() => {
+                    const scene = this.$refs[item.ref].$module.scene;
+                    const rootRegion = scene.getRootRegion();
+                    const regions = rootRegion.getChildRegions();
+                    regions.forEach(region => {
+                        region.setVisibility(false);
+                    });
+                }
+            });
+            setTimeout(() => {
                 console.log('test');
                 alert();
                 document.getElementsByClassName('el-icon-arrow-left')[0].click();
@@ -118,7 +122,7 @@ export default {
         }
     }
 }
-setTimeout (() => {
+setTimeout(() => {
     document.getElementsByClassName('el-icon-arrow-left')[0].click();
     document.querySelectorAll('.fitWindow')[0].parentElement.click();
 }, 2000);
@@ -184,67 +188,88 @@ td {
     text-align: right;
 }
 
-    .sidebar {
-        position: fixed;
-        right: 0;
-        top: 50px;
-        width: 40%;
-        height: 90%;
-        overflow: auto;
-        background-color: #f5f5f5;
-        padding: 20px;
-        box-sizing: border-box;
-        transition: transform 0.3s ease-in-out;
-        transform: translateX(100%);
-        border: 1px solid #00ff00; /* Add a border */
-        border-radius: 10px; /* Rounded corners */
-        font-size: 0.9rem;
-    }
+.sidebar {
+    position: fixed;
+    right: 0;
+    top: 50px;
+    width: 40%;
+    height: 80%;
+    overflow: auto;
+    background-color: #f5f5f5;
+    padding: 20px;
+    box-sizing: border-box;
+    transition: transform 0.3s ease-in-out;
+    transform: translateX(100%);
+    border: 1px solid #00ff00;
+    /* Add a border */
+    border-radius: 10px;
+    /* Rounded corners */
+    font-size: 0.9rem;
+}
 
 .sidebar.show {
     transform: translateX(0);
 }
 
-        .sidebar h3 {
-            color: #00ff00; /* Change the color of the header */
-            /*border-bottom: 1px solid #007BFF; /* Add a bottom border to the header */
-            padding-bottom: 10px; /* Add some padding to the bottom of the header */
-            font-size: 0.9rem;
-        }
+.sidebar h3 {
+    color: #00ff00;
+    /* Change the color of the header */
+    /*border-bottom: 1px solid #007BFF; /* Add a bottom border to the header */
+    padding-bottom: 10px;
+    /* Add some padding to the bottom of the header */
+    font-size: 0.9rem;
+}
 
-        .sidebar button {
-            background-color: #11a611; /* Change the background color of the button */
-            color: #0e090a; /* Change the text color of the button */
-            border: none; /* Remove the border of the button */
-            padding: 1px 5px; /* Add some padding to the button */
-            border-radius: 0.25px; /* Rounded corners for the button */
-            cursor: pointer; /* Change the cursor when hovering over the button */
-            font-size: 0.8rem;
-        }
+.sidebar button {
+    background-color: #11a611;
+    /* Change the background color of the button */
+    color: #0e090a;
+    /* Change the text color of the button */
+    border: none;
+    /* Remove the border of the button */
+    padding: 1px 5px;
+    /* Add some padding to the button */
+    border-radius: 0.25px;
+    /* Rounded corners for the button */
+    cursor: pointer;
+    /* Change the cursor when hovering over the button */
+    font-size: 0.8rem;
+}
 
 .sidebar button:hover {
-    background-color: #a60857; /* Change the background color of the button when hovering */
+    background-color: #a60857;
+    /* Change the background color of the button when hovering */
 }
 
 .sidebar table {
     width: 100%;
-    border-collapse: collapse; /* Remove the space between the borders of the table and cells */
-}
-.sidebar th, .sidebar td {
-    padding: 2px;
-    border: 1px solid #ddd; /* Add a border to the cells */
-    text-align: left; /* Align the text to the left */
+    border-collapse: collapse;
+    /* Remove the space between the borders of the table and cells */
 }
 
-        .sidebar th {
-            background-color: #00ff00; /* Change the background color of the header cells */
-            color: #fff; /* Change the text color of the header cells */
-            font-size: 0.8rem;
-        }
-.sidebar tr:hover {
-    background-color: #53e45f; /* Add a background color to the table rows when hovering */
+.sidebar th,
+.sidebar td {
+    padding: 2px;
+    border: 1px solid #ddd;
+    /* Add a border to the cells */
+    text-align: left;
+    /* Align the text to the left */
 }
-.sidebar tr{
+
+.sidebar th {
+    background-color: #00ff00;
+    /* Change the background color of the header cells */
+    color: #fff;
+    /* Change the text color of the header cells */
+    font-size: 0.8rem;
+}
+
+.sidebar tr:hover {
+    background-color: #53e45f;
+    /* Add a background color to the table rows when hovering */
+}
+
+.sidebar tr {
     /* first letter capital */
     text-transform: lowercase;
     /* text alignment */

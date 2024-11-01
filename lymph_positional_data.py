7e48b842608b9410a96cc23928618634af8c4a0e
Nick Young <nick.young@auckland.ac.nz>
2024-11-01 13:52:38 +1300

    show specific lymph nodes on element hover

--- lymph_positional_data.py
+++ lymph_positional_data.py
@@ -4,7 +4,7 @@ from tqdm import tqdm
 from pathlib import Path
 
 processed_data = []
-lymphs_json = Path('./public/data/all_element_jasons/Nodes/all_node_2.json')
+lymphs_json = Path('./public/data/all_element_jasons/Nodes/all_nodes_labels_2.json')
 
 if __name__ == "__main__":
     #load in lymph json data
@@ -16,6 +16,8 @@ if __name__ == "__main__":
 
     #reshape points to xyz co-ordinates
     pts = np.reshape(pts['0'], (-1, 3))
+    print(len(lbls))
+    assert pts.shape[0] == len(lbls)
 
     for idx in tqdm(range(pts.shape[0])):
         processed_data.append({'label': lbls[idx], 'position': pts[idx].tolist()})

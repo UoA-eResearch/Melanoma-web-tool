cd223e3a4cd186d16ac5afbdab0811f117e5e862
Nick Young <nick.young@auckland.ac.nz>
2024-11-11 15:40:13 +1300

    heatmaps

--- generate_human_mesh.py
+++ generate_human_mesh.py
@@ -3,38 +3,18 @@ import numpy as np
 import trimesh
 from pathlib import Path
 
-# Function to clean and reshape raw face data into standard x,y,z format
-def clean_raw_faces(faces):
-    temp = []
-    face_id = 0
-    for i in faces:
-        if face_id == 0:
-            face_id = i
-            continue
-        if face_id != 0 and i == face_id:
-            continue
-        else:
-            temp.append(i)
-    
-    reshaped = np.reshape(np.array(temp), (-1,3))
-    uniq_faces = np.unique(reshaped, axis = 0)
-    face_data = uniq_faces
-
-    return np.array(face_data)
-
 mesh_json = Path('./public/data/Heat_maps/Jsons_heat_maps_6-3-24/1_NFs_1.json')
 
 with open(mesh_json) as f:
     data = json.load(f)
 
-# Get the vertices and faces from the JSON file
-verts = np.reshape(np.array(data['vertices']), (-1, 3))
-faces = np.array(data['faces'])
-clean_faces = np.reshape(clean_raw_faces(faces), (-1,3))
-normals = np.reshape(np.array(data['normals']), (-1,3))
+verts = np.reshape(data['vertices'], (-1, 3))
+print(verts.shape)
+print(np.unique(verts, axis=0).shape)
+faces = np.reshape(data["faces"], (-1,10))
+faces = faces[:,1:4]
+print(faces.shape)
 
-mesh_json = {'name': 'human_mesh', 'vertices': verts.tolist(), 'faces': clean_faces.tolist(), 'normals': normals.tolist()}
-json.dump(mesh_json, open("human_mesh.json", 'w'))
-
-mesh = trimesh.Trimesh(vertices=verts, faces=clean_faces, normals=normals)
-mesh.export('./human_mesh.obj')
+# process=False is needed here to prevent merging vertices, which throws off the heatmaps
+mesh = trimesh.Trimesh(vertices=verts, faces=faces, process=False)
+mesh.export('./human_mesh.glb')
--- heat_maps_data_generation.py
+++ heat_maps_data_generation.py
@@ -1,65 +1,25 @@
+#!/usr/bin/env python3
 import os
 import json
 import numpy as np
 import trimesh
 from pathlib import Path
 from tqdm import tqdm
+import pandas as pd
 
-# Function to clean and reshape raw face data into standard x,y,z format
-def clean_raw_faces(faces):
-    temp = []
-    face_id = 0
-    for i in faces:
-        if face_id == 0:
-            face_id = i
-            continue
-        if face_id != 0 and i == face_id:
-            continue
-        else:
-            temp.append(i)
-    
-    reshaped = np.reshape(np.array(temp), (-1,3))
-    uniq_faces = np.unique(reshaped, axis = 0)
-    face_data = uniq_faces
+heat_maps_verts_colors = {}
+json_heatmaps_dir = Path('./public/data/')
 
-    return np.array(face_data)
+with open("public/data/heat_maps.json") as f:
+    elems = json.load(f)
 
-def rgb_int2tuple(idx, rgbint):
-    return (int(rgbint // 256 // 256 % 256), int(rgbint // 256 % 256), int(rgbint % 256), int(idx))
-
-if __name__ == "__main__":
-    heat_maps_verts_colors = []
-    json_heatmaps_dir = Path('./public/data/Heat_maps/')
-
-    for file in tqdm(os.listdir(json_heatmaps_dir)):
-        if not file.endswith('.json'):
-            continue
-
+for elem in tqdm(elems):
+    if elem["Type"] == "Surfaces":
+        name = elem.get("RegionPath", "") + " " + elem.get("GroupName", "")
+        name = name.replace("Heat Map", "").strip()
+        file = elem["URL"]
         with open(Path.joinpath(json_heatmaps_dir, file)) as f:
             data = json.load(f)
-            raw_colors = np.array(data['colors'])
-            
-            rgb = []
-            for i,c in enumerate(raw_colors):
-                RGB = rgb_int2tuple(1, c)
-                rgb.append(RGB)
-            
-            heat_maps_verts_colors.append({'name':file, 'heatmap':rgb})
-    
-    # Not all jsons are in the same directory
-    for file in os.listdir(Path.joinpath(json_heatmaps_dir, 'Jsons_heat_maps_6-3-24')):
-        if not file.endswith('.json'):
-            continue
+            heat_maps_verts_colors[name] = data['colors']
 
-        with open(Path.joinpath(json_heatmaps_dir, 'Jsons_heat_maps_6-3-24', file)) as f:
-            data = json.load(f)
-            raw_colors = np.array(data['colors'])
-            
-            rgb = []
-            for i,c in enumerate(raw_colors):
-                RGB = rgb_int2tuple(1, c)
-                rgb.append(RGB)
-            
-            heat_maps_verts_colors.append({'name':file, 'heatmap':rgb})
-
-    json.dump(heat_maps_verts_colors, open("heat_maps_verts_colors.json", 'w'))
+json.dump(heat_maps_verts_colors, open("heat_maps_verts_colors.json", 'w'))

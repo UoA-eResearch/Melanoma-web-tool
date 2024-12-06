acd2951506df2478db6ebacd65f3435b335af640
Nick Young <nick.young@auckland.ac.nz>
2024-12-06 13:04:01 +1300

    add some missing heatmaps and discrete points, color non-normalised discrete points black

--- heat_maps_data_generation.py
+++ heat_maps_data_generation.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 import pandas as pd
 
 heat_maps_verts_colors = {}
-json_heatmaps_dir = Path('./public/data/')
+json_heatmaps_dir = './public/data/'
 
 with open("public/data/heat_maps.json") as f:
     elems = json.load(f)
@@ -20,20 +20,21 @@ for elem in tqdm(elems):
         if elem["Type"] == "Surfaces":
             name = elem.get("RegionPath", "") + " " + elem.get("GroupName", "")
             name = name.replace("Heat Map", "").strip()
-            file = elem["URL"]
-            with open(Path.joinpath(json_heatmaps_dir, file)) as f:
+            file = json_heatmaps_dir + elem["URL"]
+            with open(file) as f:
                 data = json.load(f)
                 heat_maps_verts_colors[name] = data['colors']
         elif elem["Type"] == "Glyph":
             RegionPath = elem.get("RegionPath", "")
-            file = elem["URL"]
+            file = json_heatmaps_dir + elem["URL"]
             if "fre" in file:
                 RegionPath += " Frequency"
-            with open(Path.joinpath(json_heatmaps_dir, file)) as f:
+                print(RegionPath)
+            with open(file) as f:
                 data = json.load(f)
                 pos = np.reshape(data["positions"]["0"], (-1, 3)).tolist()
-                scale = np.reshape(data["scale"]["0"], (-1, 3))
-                scale = scale[:,1].tolist()
+                #scale = np.reshape(data["scale"]["0"], (-1, 3))
+                #scale = scale[:,1].tolist()
                 if RegionPath in discrete_points_normalized:
                     print(f"Duplicate {RegionPath}")
                 discrete_points_normalized[RegionPath] = {
@@ -41,8 +42,26 @@ for elem in tqdm(elems):
                     "colors": data["colors"]["0"],
                     #"scales": scale,
                 }
+            file = os.path.basename(file)
+            file = "public/data/Heat_maps/discrete_points_normalized/" + file.replace("nor", "fre").replace("_points", "").replace("_pointys", "").replace("NFs", "NFS")
+            if os.path.isfile(file) and "fre" in file:
+                RegionPath += " Frequency"
+                print(RegionPath)
+                with open(file) as f:
+                    data = json.load(f)
+                    pos = np.reshape(data["positions"]["0"], (-1, 3)).tolist()
+                    #scale = np.reshape(data["scale"]["0"], (-1, 3))
+                    #scale = scale[:,1].tolist()
+                    if RegionPath in discrete_points_normalized:
+                        print(f"Duplicate {RegionPath}")
+                    discrete_points_normalized[RegionPath] = {
+                        "positions": pos,
+                        "colors": data["colors"]["0"],
+                        #"scales": scale,
+                    }
     except Exception as e:
         print(f"{e} for {elem}")
+        raise
 
 json.dump(heat_maps_verts_colors, open("heat_maps_verts_colors.json", 'w'))
 json.dump(discrete_points_normalized, open("discrete_points_normalized.json", 'w'))

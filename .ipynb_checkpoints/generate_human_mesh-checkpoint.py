588442f63e45b25d818fee1c6932ceb3af5f7231
Tharanga Don <tjay723@UoA.auckland.ac.nz>
2025-09-10 14:15:41 +1200

    New data from david-9-4

---
+++ .ipynb_checkpoints/generate_human_mesh-checkpoint.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import json
+import numpy as np
+import trimesh
+from pathlib import Path
+
+mesh_json = Path('./public/data/Heat_maps/Jsons_heat_maps_6-3-24/1_NFs_1.json')
+
+with open(mesh_json) as f:
+    data = json.load(f)
+
+verts = np.reshape(data['vertices'], (-1, 3))
+print(verts.shape)
+print(np.unique(verts, axis=0).shape)
+faces = np.reshape(data["faces"], (-1,10))
+faces = faces[:,1:4]
+print(faces.shape)
+
+# process=False is needed here to prevent merging vertices, which throws off the heatmaps
+mesh = trimesh.Trimesh(vertices=verts, faces=faces, process=False)
+mesh.export('./human_mesh.glb')
